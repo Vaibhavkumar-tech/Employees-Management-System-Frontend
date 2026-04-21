fbb5beaffdc60d164956fe872c50ac6b13cc352e
Vaibhavkumar-tech <vaibhavkumar62078@example.com>
2026-04-21 10:13:27 +0530

    fff

--- src/others/AllTask.jsx
+++ src/others/AllTask.jsx
@@ -16,7 +16,7 @@ const AllTask = (setProfile) => {
     <div className='h-full bg-gray-900 p-10 py-20 w-150 sm:w-300 md:300 lg:w-screen lg:h-screen'>
         <h2 className=' text-2xl font-bold -mt-10 mb-15 text-emerald-500 hover:text-emerald-900'>Employees Details</h2>
         <div>
-            <div className='flex flex-row justify-between text-white rounded-2xl lg:w-300 w-100 lg:ml-40 mt-5 sm:w-150 lg:px-25 bg-emerald-500 h-15 items-center'>
+            <div className='flex flex-row justify-between text-white rounded-2xl lg:w-300 w-100 lg:ml-30 mt-5 sm:w-150 lg:px-25 bg-emerald-500 h-15 items-center'>
                 <h2 className='w-1/5 p-2 px-6 text-white font-bold '>Employee Name</h2>
                 <h3 className='w-1/6 p-2 px-6 text-white font-bold '>New Task</h3>
                 <h4 className='w-1/6 p-2 px-6 text-white font-bold '>Active</h4>
@@ -24,7 +24,7 @@ const AllTask = (setProfile) => {
                 <h4 className='w-1/6 p-2 px-6 text-white font-bold '>Failed</h4>
             </div>
         </div>
-        <div className='lg:overflow-auto h-90 w-340'>
+        <div className='lg:overflow-auto h-90 w-357 lg:-ml-7'>
             {Object.values(data.userdata.employees).map(function(e,idx){
                 return(
                     <div key={idx} className='flex flex-row justify-between text-white rounded-2xl lg:w-300 w-100 sm:w-150 lg:ml-40 mt-5 lg:px-25 border-3 border-emerald-500 h-15 items-center hover:border-red-600 hover:h-20'>
