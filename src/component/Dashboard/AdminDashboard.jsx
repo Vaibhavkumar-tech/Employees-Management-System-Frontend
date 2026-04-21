a2cee6861884861ba008e57aabb45fa880048844
Vaibhavkumar-tech <vaibhavkumar62078@example.com>
2026-04-21 09:50:17 +0530

    fix2

--- src/component/Dashboard/AdminDashboard.jsx
+++ src/component/Dashboard/AdminDashboard.jsx
@@ -7,7 +7,7 @@ import Details from '../../others/Details'
 const AdminDashboard = ({data}) => {
 
   return (
-    <div className='bg-gray-900 bg-cover bg-bg-center lg:h-192 h-400 sm:h-300 md:200 w-200 lg:w-350'>
+    <div className='bg-gray-900 bg-cover bg-bg-center lg:h-192 h-400 sm:h-300 md:200 w-200 lg:w-358'>
       <Header data={data}/>
       <CreateTask/>
       <Details data={data}/>
